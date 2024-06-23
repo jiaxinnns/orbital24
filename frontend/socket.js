191bbddee306508bd57bd1b80242936bf0865561
jia xin <jjiiaaxxiinn@gmail.com>
2024-06-23 20:05:44 +0800

    test

--- frontend/socket.js
+++ frontend/socket.js
@@ -1,7 +1,7 @@
-import { io } from "socket.io-client";
+// import { io } from "socket.io-client";
 
-const SOCKET_URL = process.env.VITE_APP_API_URL;
+// const SOCKET_URL = process.env.VITE_APP_API_URL;
 
-export const socket = io(SOCKET_URL, {
-  transports: ["websocket"],
-});
+// export const socket = io(SOCKET_URL, {
+//   transports: ["websocket"],
+// });
